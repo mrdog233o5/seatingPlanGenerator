63ac83081bf479919978fff06c507bd725807fbb
mrdog233o5 <mr.dog233o5@gmail.com>
2024-08-23 23:41:21 +0800

    made a completely random generating algorithm (no repeating)

---
+++ src/main.js
@@ -0,0 +1,121 @@
+const inputContainer = document.getElementById("inputContainer");
+const inputField = document.getElementsByClassName("inputField");
+const nameInput = document.getElementById("name");
+const genderInput = document.getElementById("gender");
+const generateBtn = document.getElementById("generateBtn");
+var names = [];
+var genders = [];
+var male = [];
+var female = [];
+var res = [];
+var x = 0;
+var y = 0;
+
+// set border
+inputField[inputField.length - 1].style.border = "none";
+
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
+function readData() {
+	// read data
+	names = nameInput.value.split("\n");
+	genders = genderInput.value.split("\n");
+
+	names = `Samuel
+Rishi
+Ian
+Jade
+William
+Taliya
+Keira
+Clementine
+Tiffany
+Hendre
+Jerry
+Hin Ching
+Hin Lung
+Yuzhi
+Valerie
+Hailey
+David
+Lucy
+Adianna
+Dani
+Louie
+Belle
+Winston`.split("\n");
+	genders = `m
+m
+m
+f
+m
+f
+f
+f
+f
+m
+m
+f
+m
+m
+f
+f
+m
+f
+f
+m
+m
+f
+m`.split("\n");
+	male = [];
+	female = [];
+	res = [];
+
+	names.forEach((name, i) => {
+		if (genders[i] == "m") {
+			male.push(name);
+		} else if (genders[i] == "f") {
+			female.push(name);
+		}
+	});
+
+	x = document.getElementById("x").value;
+	y = document.getElementById("y").value;
+	x = 23;
+	y = 1;
+	var tempArr = [];
+	for (let i = 0; i < x; i++) {
+		tempArr.push("");
+	}
+	for (let i = 0; i < y; i++) {
+		res.push(tempArr);
+	}
+}
+
+function generate() {
+	var tempName = "";
+	for (let i = 0; i < x; i++) {
+		// every row
+		for (let j = 0; j < y; j++) {
+			// every seat
+			tempName = names[Math.floor(Math.random()*names.length)];
+			names.remove(tempName);
+			res[j][i] = tempName;
+		}
+	}
+	console.log(res);
+}
+
+generateBtn.onclick = () => {
+	readData();
+	generate();
+}
